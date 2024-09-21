9807304d098921e98a7fc8580a1c84b15a5fbdfb
Guillermo Nunez <guillermo6001@gmail.com>
2024-09-21 10:07:02 -0400

    First commit

---
+++ bin/client_server_example.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:socket_io_client/socket_io_client.dart' as socket_io_client;
+import 'package:socket_io/socket_io.dart';
+
+Future<void> main(List<String> args) async {
+  if (args.isEmpty) {
+    print("No args passed");
+    print("Usage:");
+    print("Server: dart bin/client_server_example.dart s");
+    print("Client: dart bin/client_server_example.dart c");
+    return;
+  }
+  if (args.first == "s") _server();
+  if (args.first == "c") _client();
+}
+
+StreamSubscription<String>? streamSubscription;
+
+Future<void> _server() async {
+  final server = Server();
+
+  server.on('connection', (client) {
+    // cuando un cliente se conecta pasa por aquí
+
+    final id = client.id.toString();
+    print('Connected id: $id');
+    client.broadcast.emit("msg", 'El id: $id se ha unido a la sala');
+
+    client.on('stream', (data) {
+      print('$id dice: $data');
+    });
+
+    client.on(
+        'disconnect', // el canal disconnect es cuando se desconecta el cliente del servidor
+        (_) {
+      print('El cliente $id se ha desconectado del server');
+      client.broadcast.emit("msg", 'El id: $id se a ido!');
+    });
+
+    Timer(Duration(seconds: 5), () {
+      client.emit('msg', 'Hi!, from server');
+    });
+  });
+
+  server.listen(3000);
+
+  streamSubscription =
+      readline().listen((String line) => server.emit('msg', line));
+  // puedo hacer esto porque lo está utilizando un solo subscritor a la vez
+  // pero en caso que el mismo stream lo necesiten dos susbcritores a la vez
+  // allí creo que puede originar un error, y se necesiatn stremController
+  // el tema es que la entrada del teclado me genera es un stream, entonces
+  // en éste caso en específico sería así
+  // porque
+}
+
+Future<void> _client() async {
+  final client = socket_io_client.io('http://localhost:3000', <String, dynamic>{
+    'transports': ['websocket'],
+  });
+  // cuando se ejecuta ésta istrucción él se queda esperando hasta conectarse
+
+  client.onConnect((_) {
+    print('Connected');
+
+    streamSubscription = readline().listen((String line) {
+      client.emit('stream', line);
+    });
+  });
+
+  client.onDisconnect((_) {
+    print('Se ha desconectado del servidor');
+
+    streamSubscription?.cancel();
+    client.dispose();
+  });
+
+  client.on('msg', (data) => _printFromServer(data));
+}
+
+Stream<String> readline() =>
+    stdin.transform(latin1.decoder).transform(const LineSplitter());
+
+// Stream<String> readline() =>
+//     stdin.transform(utf8.decoder).transform(const LineSplitter());
+
+void _printFromServer(String message) => print(message);
---
+++ test/client_server_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:client_server_example/client_server_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
